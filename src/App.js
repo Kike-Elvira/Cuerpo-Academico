89ce24422439949439e704918618464c41096f10
Kike Elvira <joseelvirarivas01@gmail.com>
2023-11-30 16:00:40 -0600

    laptop desing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/login/Login';
-import CaForm from './components/CaForm/CaForm';
-import ProfForm from './components/ProfForm/ProfForm';
-import ProjectForm from './components/ProjectForm/ProjectForm';
+import "./App.css";
+import Login from "./components/login/Login";
+import CaForm from "./components/CaForm/CaForm";
+import ProfForm from "./components/ProfForm/ProfForm";
+import ProjectForm from "./components/ProjectForm/ProjectForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-   {/*<Login></Login>*/}
-  {/* <CaForm></CaForm>*/}
-    <ProjectForm></ProjectForm>
-   {/*   <ProfForm></ProfForm> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/project" element={<ProjectForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/ca" element={<CaForm />} />
+        <Route path="/prof" element={<ProfForm />} />
+        {/* Ruta por defecto, redirige a /login */}
+        <Route index element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
